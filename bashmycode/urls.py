71cdadb822b7700ada8838d6f18f9a1baaada448
Liam Halpin <2383998H@student.gla.ac.uk>
2020-03-18 13:55:51 +0000

    mapped urls for login, logout and profile page

--- bashmycode/urls.py
+++ bashmycode/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 from bashmycode import views
 
-from django.contrib.auth import authenticate, login
-from django.http import HttpResponse
+from django.contrib.auth import views as auth_views
 from django.urls import reverse
 from django.shortcuts import redirect
 
@@ -12,10 +11,11 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('help/', views.help, name='help'),
     path('register/', views.register, name='register'),
-    
-    path('login/', views.user_login, name='login'),
-    path('restricted/', views.restricted, name='restricted'),
-    path('logout/', views.user_logout, name='logout'),
+
+    path('login/', auth_views.LoginView.as_view(template_name='bashmycode/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='bashmycode/logout.html'), name='logout'),
+
+    path('profile/', views.profile, name='profile'),
 
     path('bash/', views.bash, name='bash'),
     path('help/', views.help, name='help')
