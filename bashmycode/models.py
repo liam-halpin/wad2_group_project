ce1865e1d713f46564caa07a52c0da90132a0057
Liam Halpin <2383998H@student.gla.ac.uk>
2020-03-19 09:43:19 +0000

    when user registers, profile automatically created

--- bashmycode/models.py
+++ bashmycode/models.py
@@ -1,14 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class UserProfile(models.Model):
+class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-
-    # Additional attributes to include
-    picture = models.ImageField(upload_to='profile_images', blank=True)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
 
     def __str__(self):
-        return self.user.username
+        return f'{self.user.username}\'s Profile'
+
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
