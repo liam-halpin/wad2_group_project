049bfd5fdbbf9e7655bd037e4724ef1c3ac37ec1
Liam Halpin <2383998H@student.gla.ac.uk>
2020-03-22 14:16:10 +0000

    re-configured urls that map to bash.html and help.html

--- bashmycode/urls.py
+++ bashmycode/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, reverse
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+from .views import PostListViewHelp, PostListViewBash, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 from bashmycode import views
 from django.contrib.auth import views as auth_views
 from django.shortcuts import redirect
@@ -19,10 +19,11 @@ urlpatterns = [
 
     path('profile/', views.profile, name='profile'),
 
-    path('bash/', PostListView.as_view(), name='bash'),
+    path('bash/', PostListViewBash.as_view(), name='bash'),
+    path('help/', PostListViewHelp.as_view(), name='help'),
+    
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
-    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
-    path('help/', PostListView.as_view(), name='help')
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
