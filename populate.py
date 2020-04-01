89b318001e2645ea3dcd3e814c9d0c34cd3356ce
mohibfd <2431135A@student.gla.ac.uk>
2020-04-01 21:24:28 +0100

    population script complete

--- populate.py
+++ populate.py
@@ -1,37 +1,15 @@
 import os
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tango_with_django_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wad2_group_project.settings')
 
 import django
 django.setup()
 from bashmycode.models import Post
 
 def populate():
-    other_pages = [
-        {'title':'Bottle',
-         'url':'http://bottlepy.org/docs/dev/',
-         'views': 20},
-        {'title':'Flask',
-         'url':'http://flask.pocoo.org',
-         'views': 58}]
-         
-    cats = {'Other Frameworks': {'pages': other_pages, 'views': 32, 'likes': 16}}
 
-    for cat, cat_data in cats.items():
-        c = add_cat(cat, cat_data['views'], cat_data['likes'])
-        for p in cat_data['pages']:
-                add_page(c, p['title'], p['url'], p['views'])
-
-    for c in Category.objects.all():
-        for p in Page.objects.filter(category=c):
-            print(f'- {c}: {p}')
-
-def add_page(cat,title,url,views=0):
-    p = Page.objects.get_or_create(category=cat, title=title)[0]
-    p.url=url
-    p.views=views
-    p.save()
-    return p
+    for c in Post.objects.all():
+            print(f'- {c}')
     
 def add_cat(name, views, likes):
     c = Post.objects.get_or_create(name=name)[0]
