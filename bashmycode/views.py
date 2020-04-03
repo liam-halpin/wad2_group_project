832d7a62bbb14248c953e9a9cab446c7cd284abb
Liam Halpin <2383998H@student.gla.ac.uk>
2020-04-03 13:43:32 +0100

    fixing queries

--- bashmycode/views.py
+++ bashmycode/views.py
@@ -38,7 +38,7 @@ class UserPostListView(ListView):
 
     def get_queryset(self):
         user = get_object_or_404(User, username=self.kwargs.get('username'))
-        return Post.objects.filter(author=user).order_by('-date_posted')
+        return Post.objects.filter(author=user).order_by('-likes')
 
 class PostDetailView(DetailView):
     model = Post
